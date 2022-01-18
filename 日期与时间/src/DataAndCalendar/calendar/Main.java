ea33bf35a8d21a2272abadd21700c652d4395948
Jasmine521 <1779605057@qq.com>
2022-01-18 14:43:14 +0800

    日期与时间

--- 日期与时间/src/DataAndCalendar/calendar/Main.java
+++ 日期与时间/src/DataAndCalendar/calendar/Main.java
@@ -1,7 +1,43 @@
 package DataAndCalendar.calendar;
 
+import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
-
+        Calendar c = Calendar.getInstance();
+        int y = c.get(Calendar.YEAR);
+        int m = 1 + c.get(Calendar.MONTH);
+        int d = c.get(Calendar.DAY_OF_MONTH);
+        int w = c.get(Calendar.DAY_OF_WEEK);
+        int hh = c.get(Calendar.HOUR_OF_DAY);
+        int mm = c.get(Calendar.MINUTE);
+        int ss = c.get(Calendar.SECOND);
+        int ms = c.get(Calendar.MILLISECOND);
+        System.out.println(y + "-" + m + "-" + d + " " + w + " " + hh + ":" + mm + ":" + ss + "." + ms);
+        c.clear();
+        // 设置2019年:
+        c.set(Calendar.YEAR, 2019);
+        // 设置9月:注意8表示9月:
+        c.set(Calendar.MONTH, 8);
+        // 设置2日:
+        c.set(Calendar.DATE, 2);
+        // 设置时间:
+        c.set(Calendar.HOUR_OF_DAY, 21);
+        c.set(Calendar.MINUTE, 22);
+        c.set(Calendar.SECOND, 23);
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(c.getTime()));
+        TimeZone tzDefault = TimeZone.getDefault(); // 当前时区
+        TimeZone tzGMT9 = TimeZone.getTimeZone("GMT+09:00"); // GMT+9:00时区
+        TimeZone tzNY = TimeZone.getTimeZone("America/New_York"); // 纽约时区
+        System.out.println(tzDefault.getID()); // Asia/Shanghai
+        System.out.println(tzGMT9.getID()); // GMT+09:00
+        System.out.println(tzNY.getID()); // America/New_York
+        List<String> tzs = new ArrayList(Arrays.asList(TimeZone.getAvailableIDs()));
+        for(Iterator<String> it = tzs.iterator();it.hasNext();){
+            System.out.println(it.next());
+        }
     }
 }
