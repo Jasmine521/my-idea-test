a9a29a9530e6f68b7ed4af119b876a580719857f
Jasmine521 <1779605057@qq.com>
2022-01-20 14:07:58 +0800

    同步方法

--- 多线程/src/tcbTongBu/Main.java
+++ 多线程/src/tcbTongBu/Main.java
@@ -1,5 +1,14 @@
 package tcbTongBu;
 
+/**
+ * 用synchronized修饰方法可以把整个方法变为同步代码块，synchronized方法加锁对象是this；
+ * <p>
+ * 通过合理的设计和数据封装可以让一个类变为“线程安全”；
+ * <p>
+ * 一个类没有特殊说明，默认不是thread-safe；
+ * <p>
+ * 多线程能否安全访问某个非线程安全的实例，需要具体问题具体分析。
+ */
 public class Main {
     public static void main(String[] args) throws Exception {
         AddThread add = new AddThread();
@@ -13,6 +22,11 @@ public class Main {
     }
 }
 
+class Pair {
+    public int first;
+    public int last;
+}
+
 class Counter {
     public static final Object lock = new Object();
     public static volatile int count = 0;
@@ -20,6 +34,38 @@ class Counter {
     public static volatile int sor = 0;
     public static volatile int Aj = 0;
     public static volatile int Bj = 0;
+
+    public void add(int n) {
+        synchronized (this) {       //线程安全的方法
+            count += n;
+        }
+    }
+
+    public synchronized void add1(int n) {      //线程安全的方法
+
+        count += n;
+
+    }
+
+    public void dec(int n) {
+        synchronized (this) {
+            count -= n;
+        }
+    }
+
+    public int get() {
+        return count;
+    }
+
+    private int first;
+    private int last;
+
+    public Pair getpair() {
+        Pair p = new Pair();
+        p.first = first;
+        p.last = last;
+        return p;
+    }
 }
 
 class AddThread extends Thread {
